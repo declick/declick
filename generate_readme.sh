9d5e7e6afb183b0a796dfc6d1aec97f5933db3e2
declick <36468501+declick@users.noreply.github.com>
2023-08-05 16:32:19 +0200

    Update generate_readme.sh

--- generate_readme.sh
+++ generate_readme.sh
@@ -4,4 +4,5 @@ date_reference="2018-02-13"
 date_actuelle=$(date -u "+%Y-%m-%d")
 jours=$(( ($(date -d $date_actuelle +%s) - $(date -d $date_reference +%s)) / 86400 ))
 
-cat template/README.md.tpl | sed "s/{{DAYS_SINCE_CREATION}}/$jours/" > README.md
+# Remplacer le nombre de jours dans le fichier README.md
+sed -i "s/Le projet a été créé le [0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\} soit [0-9]\+ jours\./Le projet a été créé le $date_reference soit $jours jours./" README.md
