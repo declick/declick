8a8c4f404a77d76f5c4c40dd2bed3247acbb6d2b
declick <36468501+declick@users.noreply.github.com>
2023-08-05 16:16:48 +0200

    Update generate_readme.sh

--- generate_readme.sh
+++ generate_readme.sh
@@ -1,8 +1,7 @@
+#!/bin/bash
 
 date_reference="2018-02-13"
 date_actuelle=$(date -u "+%Y-%m-%d")
 jours=$(( ($(date -d $date_actuelle +%s) - $(date -d $date_reference +%s)) / 86400 ))
 
-cat << EOF > README.md
-Le projet a été créé le 2018-02-13 soit $jours jours.
-EOF
+cat template/README.md.tpl | sed "s/{{DAYS_SINCE_CREATION}}/$jours/" > README.md
